6d339538310c078f5bc35ce4e91b3fef7c9c7f92
Rohith M <rohith.m068@gmail.com>
2023-06-17 00:29:04 +0530

    aws_ssm added

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_ssm_parameter" "param"
+{
+  count = length(var.params)
+  name = var.params[count.index].name
+  type = var.params[count.index].type
+  value = var.params[count.index].value
+}
+
+variable "params" {
+  default = [
+    { name = "roboshop.dev.frontend.catalogue_url", value="http://catalogue-dev.rohdevops.online",type="String"},
+    { name = "roboshop.dev.frontend.cart_url", value="http://cart-dev.rohdevops.online",type="String"},
+    { name = "roboshop.dev.frontend.user_url", value="http://user-dev.rohdevops.online",type="String"},
+    { name = "roboshop.dev.frontend.payment_url", value="http://payment-dev.rohdevops.online",type="String"},
+    { name = "roboshop.dev.frontend.shipping_url", value="http://shipping-dev.rohdevops.online",type="String"}]
+}
