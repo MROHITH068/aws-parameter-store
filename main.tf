2c9f0264f04b44c60eec20db7448c7c6d05f67f3
Rohith M <rohith.m068@gmail.com>
2023-06-17 14:59:26 +0530

    roboshop.conf

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ resource "aws_ssm_parameter" "param" {
 
 variable "params" {
   default = [
-    { name = "roboshop.dev.frontend.catalogue_url", value="http://catalogue-dev.rohdevops.online",type="String"},
-    { name = "roboshop.dev.frontend.cart_url", value="http://cart-dev.rohdevops.online",type="String"},
-    { name = "roboshop.dev.frontend.user_url", value="http://user-dev.rohdevops.online",type="String"},
-    { name = "roboshop.dev.frontend.payment_url", value="http://payment-dev.rohdevops.online",type="String"},
-    { name = "roboshop.dev.frontend.shipping_url", value="http://shipping-dev.rohdevops.online",type="String"}]
+    { name = "roboshop.dev.frontend.catalogue_url", value="http://catalogue-dev.rohdevops.online:8080/",type="String"},
+    { name = "roboshop.dev.frontend.cart_url", value="http://cart-dev.rohdevops.online:8080/",type="String"},
+    { name = "roboshop.dev.frontend.user_url", value="http://user-dev.rohdevops.online:8080/",type="String"},
+    { name = "roboshop.dev.frontend.payment_url", value="http://payment-dev.rohdevops.online:8080/",type="String"},
+    { name = "roboshop.dev.frontend.shipping_url", value="http://shipping-dev.rohdevops.online:8080/",type="String"}]
 }
