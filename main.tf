4ec7999beb02f6b28dd6ad8416211152fb6f4e5d
Rohith M <rohith.m068@gmail.com>
2023-09-18 01:35:25 -0500

    Updated user.redis_host with elasticache endpoint

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ variable "params" {
     { name = "roboshop.dev.shipping.cart_endpoint", value="cart-dev.rohdevops.online:8080",type="String"},
 
     { name = "roboshop.dev.user.mongo_url", value="mongodb://mongodb-dev.rohdevops.online:27017/users",type="String"},
-    { name = "roboshop.dev.user.redis_host", value="redis-dev.rohdevops.online",type="String"},
+    { name = "roboshop.dev.user.redis_host", value="elasticache-dev.krhend.clustercfg.use1.cache.amazonaws.com",type="String"},
     { name = "roboshop.dev.user.mongo", value="true",type="String"},
 
     { name = "roboshop.dev.rabbitmq.amqp_user", value = "roboshop", type = "String" },
