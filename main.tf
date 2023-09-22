0254413bee4a2bae2d5d7a8953bf9bff9abae028
Rohith M <rohith.m068@gmail.com>
2023-09-22 13:58:40 -0500

    Creating prod environment in aws-parameter store

--- main.tf
+++ main.tf
@@ -54,6 +54,49 @@ variable "params" {
 #    mongodb://roboshop:roboshop123@docdb-dev.cluster-cflw1vmkzsr2.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false
 
 
+### Production
+    { name = "roboshop.prod.frontend.catalogue_url", value="http://catalogue-prod.rohdevops.online/",type="String"},
+    { name = "roboshop.prod.frontend.cart_url", value="http://cart-prod.rohdevops.online/",type="String"},
+    { name = "roboshop.prod.frontend.user_url", value="http://user-prod.rohdevops.online/",type="String"},
+    { name = "roboshop.prod.frontend.payment_url", value="http://payment-prod.rohdevops.online/",type="String"},
+    { name = "roboshop.prod.frontend.shipping_url", value="http://shipping-prod.rohdevops.online/",type="String"},
+
+    { name = "roboshop.prod.catalogue.mongo", value="true",type="String"},
+    #    { name = "roboshop.prod.catalogue.mongo_url", value="mongodb://mongodb-prod.rohdevops.online:27017/catalogue",type="String"},
+
+    { name = "roboshop.prod.cart.catalogue_port", value = "80", type = "String" },
+    { name = "roboshop.prod.cart.redis_host", value = "elasticache-prod.krhend.clustercfg.use1.cache.amazonaws.com", type = "String" },
+    { name = "roboshop.prod.cart.catalogue_host", value = "catalogue-prod.rohdevops.online", type = "String" },
+
+    { name = "roboshop.prod.payment.cart_url", value="cart-prod.rohdevops.online",type="String"},
+    { name = "roboshop.prod.payment.cart_port", value="80",type="String"},
+    { name = "roboshop.prod.payment.user_url", value="user-prod.rohdevops.online",type="String"},
+    { name = "roboshop.prod.payment.user_port", value="80",type="String"},
+    { name = "roboshop.prod.payment.amqp_host", value="rabbitmq-prod.rohdevops.online",type="String"},
+    { name = "roboshop.prod.payment.amqp_user", value="roboshop",type="String"},
+
+    { name = "roboshop.prod.shipping.db_host", value="rds-prod.cluster-cflw1vmkzsr2.us-east-1.rds.amazonaws.com",type="String"},
+    { name = "roboshop.prod.shipping.cart_endpoint", value="cart-prod.rohdevops.online:80",type="String"},
+
+    #    { name = "roboshop.prod.user.mongo_url", value="mongodb://mongodb-prod.rohdevops.online:27017/users",type="String"},
+    { name = "roboshop.prod.user.redis_host", value="elasticache-prod.krhend.clustercfg.use1.cache.amazonaws.com",type="String"},
+    { name = "roboshop.prod.user.mongo", value="true",type="String"},
+    { name = "roboshop.prod.rabbitmq.amqp_user", value = "roboshop", type = "String" },
+    { name = "roboshop.prod.mysql.username", value = "roboshop", type = "String" },
+    { name = "roboshop.prod.docdb.username", value = "roboshop", type = "String" },
+    { name = "roboshop.prod.mysql.endpoint", value = "rds-prod.cluster-cflw1vmkzsr2.us-east-1.rds.amazonaws.com", type = "String" },
+
+
+    #password is given manually.
+    { name = "roboshop.prod.payment.amqp_password", value="roboshop123",type="SecureString"},
+    { name = "roboshop.prod.rabbitmq.amqp_pass", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.prod.mysql.password", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.prod.docdb.password", value = "roboshop123", type = "SecureString" },
+    { name = "roboshop.prod.catalogue.mongo_url", value=" mongodb://roboshop:roboshop123@docdb-prod.cluster-cflw1vmkzsr2.us-east-1.docdb.amazonaws.com:27017/catalogue?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false",type="SecureString"},
+    { name = "roboshop.prod.catalogue.docdb_endpoint", value="docdb-prod.cluster-cflw1vmkzsr2.us-east-1.docdb.amazonaws.com",type="String"},
+    { name = "roboshop.prod.user.mongo_url", value=" mongodb://roboshop:roboshop123@docdb-prod.cluster-cflw1vmkzsr2.us-east-1.docdb.amazonaws.com:27017/users?tls=true&tlsCAFile=global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false",type="SecureString"},
+    { name = "roboshop.prod.user.docdb_endpoint", value="docdb-prod.cluster-cflw1vmkzsr2.us-east-1.docdb.amazonaws.com",type="String"},
+
   ]
 
 }
