69101f08fe98a483c97844c0c20b95988cff8930
Rohith M <rohith.m068@gmail.com>
2023-11-01 18:39:56 -0500

    Added Update Parameters in Jenkinsfile in roboshop-ansible and added new ssm parameters.

--- main.tf
+++ main.tf
@@ -100,6 +100,14 @@ variable "params" {
     # nexus
     { name = "roboshop.nexus.username", value="admin",type="SecureString"},
     { name = "roboshop.nexus.password", value="admin123",type="SecureString"},
+    { name = "roboshop.prod.payment.app_version", value = "1.0.0", type = "String" },
+    { name = "roboshop.prod.cart.app_version", value = "1.0.0", type = "String" },
+    { name = "roboshop.prod.user.app_version", value = "1.0.0", type = "String" },
+    { name = "roboshop.prod.catalogue.app_version", value = "1.0.0", type = "String" },
+    { name = "roboshop.prod.frontend.app_version", value = "1.0.0", type = "String" },
+    { name = "roboshop.prod.shipping.app_version", value = "1.0.0", type = "String" },
+
+
   ]
 
 }
